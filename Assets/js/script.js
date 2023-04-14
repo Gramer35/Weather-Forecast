f659ac3c5dd7cd97f0eb0e8b9bc393fce7b8c311
Chris Gramer <gramer3514@gmail.com>
2023-04-14 11:16:55 -0700

    updating search storage/warning

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,23 @@
-const citySearch = $('citySearch');
+const citySearch = $('#citySearch');
+const cityInput = $('#cityInput');
+
+
+function cityFetch(event) {
+    event.preventDefault();
+    console.log('working');
+
+    const city = cityInput.value;
+    
+    if (city === "") {
+        displayMessage('error', 'Please enter a city!');
+    };
+
+    localStorage.setItem('city', city);
+
+};
 
 
 
 
 
-
-citySearch.on('click', cityFetch)
+citySearch.on('click', cityFetch);
